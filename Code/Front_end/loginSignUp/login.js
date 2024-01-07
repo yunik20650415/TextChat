f8cd7eb16535c538e20329c9ec7ec15a6dd0e146
yunik20650415 <131844448+yunik20650415@users.noreply.github.com>
2024-01-07 21:14:46 +0545

    Add files via upload

---
+++ Code/Front_end/loginSignUp/login.js
@@ -0,0 +1,3 @@
+document.getElementById("new_account").onclick=function(){
+    window.location.href = 'signup.html';
+}
---
+++ Code/Front_end/loginSignUp/signup.js
@@ -0,0 +1,3 @@
+document.getElementById("login").onclick=function(){
+    window.location.href = 'login.html';
+}
